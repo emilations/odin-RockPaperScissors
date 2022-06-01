51f0dcf48bd2a0e740a6a67ab5de23c6996a4b13
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-01 13:23:29 -0400

    Add playRound() solution

--- script.js
+++ script.js
@@ -15,6 +15,31 @@ function computerPlay () {
     return computerSelection;
 }
 
+function playRound(playerSelection, computerSelection) {
+    let result;
+    if (playerSelection == computerSelection) {
+        return "It is a tie";
+    }
+    if ((playerSelection+computerSelection) == "RockPaper" || (playerSelection+computerSelection) == "PaperRock") {
+        if (playerSelection == "Paper") {
+            return "You win, Paper beats Rock";
+        } else {
+            return "You lose, Paper beats Rock"
+        }
+    } else if ((playerSelection+computerSelection) == "RockScissor" || (playerSelection+computerSelection) == "ScissorRock") {
+        if (playerSelection == "Rock") {
+            return "You win, Rock beats Scissors";
+        } else {
+            return "You lose, Rock beats Scissors"
+        }
+    } else {
+        if (playerSelection == "Scissor") {
+            return "You win, Scissors beats Paper";
+        } else {
+            return "You lose, Scissors beats Paper";
+        }
+    }
+    
+}
 
-
-console.log(computerPlay());
+console.log(playRound("Paper", computerPlay()))
