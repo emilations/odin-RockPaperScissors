beeabf58b8206e6cda9cdd6e2b6d745588779ca2
Emile Mouannes <emile.mouannes@outlook.com>
2022-06-01 16:21:35 -0400

    Add curratedSelection()

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+for (let i =  0; i < 5; i++) {
+    let playerSelection;
+    while(!playerSelection) {
+        playerSelection = prompt("Enter your selection: ");
+    }
+    console.log(playerSelection)
+    playerSelection = currateSelection(playerSelection);
+    break;
+}
+
+// This randomizes the hand of the computer
 function computerPlay () {
     let computerSelection;
     let random = Math.ceil(Math.random()*3);
@@ -15,6 +26,7 @@ function computerPlay () {
     return computerSelection;
 }
 
+// This simulates one round of User vs Computer
 function playRound(playerSelection, computerSelection) {
     let result;
     if (playerSelection == computerSelection) {
@@ -26,14 +38,14 @@ function playRound(playerSelection, computerSelection) {
         } else {
             return "You lose, Paper beats Rock"
         }
-    } else if ((playerSelection+computerSelection) == "RockScissor" || (playerSelection+computerSelection) == "ScissorRock") {
+    } else if ((playerSelection+computerSelection) == "RockScissors" || (playerSelection+computerSelection) == "ScissorsRock") {
         if (playerSelection == "Rock") {
             return "You win, Rock beats Scissors";
         } else {
             return "You lose, Rock beats Scissors"
         }
     } else {
-        if (playerSelection == "Scissor") {
+        if (playerSelection == "Scissors") {
             return "You win, Scissors beats Paper";
         } else {
             return "You lose, Scissors beats Paper";
@@ -42,4 +54,10 @@ function playRound(playerSelection, computerSelection) {
     
 }
 
-console.log(playRound("Paper", computerPlay()))
+// Lower case the user input
+function currateSelection(playerSelection) {
+    selectionLowerCase = playerSelection.toLowerCase().split("");
+    selectionLowerCase[0] = selectionLowerCase[0].toUpperCase();
+    selectionLowerCase = selectionLowerCase.join("");
+    console.log(selectionLowerCase);
+}
